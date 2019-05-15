21299a007972079a68371332f8e17d94e9d917fe
alejosgi <alejosgi@gmail.com>
2019-05-14 22:48:03 -0300

    Comentario para nuevos ejercicios

--- ej03b_base/ej03b_base.ino
+++ ej03b_base/ej03b_base.ino
@@ -19,13 +19,18 @@ Condiciones:
 •  PIN 9 = Detector de presencia (SW2) (1 = detección)
 •  PIN 10 = Destello de sistema funcionando (Utilizar este PIN como
 verificación visual de que el sistema está funcionando)
+
 NIVEL 1:
 Si está oscuro (SW1 == 1) y se detectó presencia (SW2 == 1), encender el
 reflector (K1 = 1) durante un tiempo determinado (fijo) y luego de dicho
 tiempo apagarlo.
+
 NIVEL 2:
 - Si se presionar el BOTON_COMANDO_MANUAL, debe encender el reflector si estaba apagado, y apagarlo si estaba encendido.
 Se debe apagar de manera temporizada únicamente si se encendió de manera automática.
+
+- Guardar en la memoria no volatil, los valores registrados (cantidad de encendidos + tiempo acumulado de funcionamiento)
+- Utilizar INPUT_PULLUP para las entradas (así evitamos la necesidad de resistencias pull up externas)
 */
 
 #define PIN_BOTON                               7
@@ -45,7 +50,7 @@ Se debe apagar de manera temporizada únicamente si se encendió de manera autom
 #define APAGAR_REFLECTOR                        digitalWrite(PIN_REFLECTOR, LOW)
 #define ENCENDER_REFLECTOR                      digitalWrite(PIN_REFLECTOR, HIGH)
 
-#define PIN_LED_TEST                            10
+#define PIN_LED_TEST                            13       //Build in Led
 #define CONFIGURAR_LED_TEST                     pinMode(PIN_LED_TEST, OUTPUT)
 #define APAGAR_LED_TEST                         digitalWrite(PIN_LED_TEST, LOW)
 #define ENCENDER_LED_TEST                       digitalWrite(PIN_LED_TEST, HIGH)
